1991cd7eb52fab09cc8a0b77d9b53d66f6458b66
aayaichetemam <aa_yaichetemam@estin.dz>
2025-11-26 16:31:24 +0100

    fix

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
 printf("================================\n");
-printf("|||welcome to This Calculator|||\n");
+printf("|||welcome to This calculator|||\n");
 printf("================================\n");
 return 0;
 }
