da4246d4fd521919d68534564b49fa6c5cb22128
aayaichetemam <aa_yaichetemam@estin.dz>
2025-11-27 23:42:34 +0100

    fi

--- mylib.h
+++ mylib.h
@@ -3,6 +3,35 @@
 
 // Functions declaration
 int add(int a, int b);
-int multiply(int a, int b);
+int subtract(int a, int b);
+int divide(int a, int b);
+int modulus(int a, int b);
+int pow_int(int base, int exp);
+int factorial(int n);
+int gcd(int a, int b);
+int lcm(int a, int b);
+int max(int a, int b);
+int min(int a, int b);
+int abs_val(int a);
+int square(int a);
+int cube(int a);
+int sqrt_int(int a);
+int log_int(int a, int base);
+int is_prime(int n);
+int cbrt_int(int a);
+float fabs_val(float x);
+float sin_int(int degrees);
+float cos_int(int degrees);
+float tan_int(int degrees);
+float loge_approx(float a);
+float exp_float(float x);
+float sqrt_float(float a);
+float pow_float(float base, int exp);
+float divide_float(float a, float b);
+float modulus_float(float a, float b);
+float cbrt_float(float a);
+float degrees_to_radians(float deg);
+float radians_to_degrees(float rad);
+
 
 #endif 
